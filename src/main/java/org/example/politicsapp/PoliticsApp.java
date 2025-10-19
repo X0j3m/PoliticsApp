f7123487927c2f4ecfe0abb332db02cbd35925f8
Michał Ptasznik <michal.ptasznik@protonmail.com>
2025-10-19 11:34:54 +0200

    First commit

---
+++ src/main/java/org/example/politicsapp/PoliticsApp.java
@@ -0,0 +1,13 @@
+package org.example.politicsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticsApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliticsApp.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/politicsapp/PoliticsAppTests.java
@@ -0,0 +1,13 @@
+package org.example.politicsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliticsAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
